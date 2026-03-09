a4150248fb121a2bcc1b368b3ee244bf62d5b744
VTM-Planfarm <vinh@planfarm.com.au>
2026-03-10 07:39:39 +0800

    Fix output messages in subset_soil.R for improved clarity; Add writing to file

--- soil-type/subset_soil.R
+++ soil-type/subset_soil.R
@@ -7,7 +7,7 @@ if (list.files("soil-type/base") |> length() == 0) {
 }
 
 base_path <- Sys.glob("soil-type/base/*.gpkg")
-cat("Reading soil type data from:", base_path)
+cat("Reading soil type data from:", base_path, "\n")
 soil_type <- read_sf(base_path) |> st_make_valid() |> st_transform(4326)
 cat(
     "Successfully read soil type data with",
@@ -20,13 +20,13 @@ if (list.files("soil-type/input") |> length() == 0) {
 }
 
 input_path <- Sys.glob("soil-type/input/*.shp")
-cat("Reading input data from:", input_path)
+cat("Reading input data from:", input_path, "\n")
 sf_input <- read_sf(input_path) |> st_make_valid() |> st_transform(4326)
-cat(paste(
+cat(
     "Successfully read input data with",
     nrow(sf_input),
     "features.\n"
-))
+)
 
 cat("Filtering soil type data...\n")
 subset_soil <- st_filter(soil_type, sf_input)
@@ -43,3 +43,11 @@ cat(
     nrow(snipped_sf),
     "features.\n"
 )
+
+if (dir.exists("soil-type/output") == FALSE) {
+    cat("Output directory does not exist. Creating 'soil-type/output'...\n")
+    dir.create("soil-type/output")
+}
+cat("Writing output to 'soil-type/output/subset_soil.geojson'...\n")
+write_sf(snipped_sf, "soil-type/output/subset_soil.geojson")
+cat("Successfully wrote output to 'soil-type/output/subset_soil.geojson'.\n")
